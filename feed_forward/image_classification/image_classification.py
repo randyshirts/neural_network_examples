029b5ce8bd0548a933cb0680b45395a3f1076f91
randyshirts <randy.shirts@gmail.com>
2022-12-19 21:46:22 -0700

    Added manual feed_forward network training with image classification

---
+++ feed_forward/image_classification/image_classification.py
@@ -0,0 +1,140 @@
+import argparse
+import numpy as np
+import importlib
+import matplotlib.pyplot as plt
+import yaml
+from dnn_utils import load_data
+
+
+def show_example_image(index=15, training_data=None, training_labels=None, classifications=None):
+    # Show an example picture
+    # Example of a picture
+    plt.imshow(training_data[index])
+    plt.show()
+    
+    print ("y = " + str(training_labels[0,index]) + ". It's a " + classifications[training_labels[0,index]].decode("utf-8") +  " picture.")
+    
+
+def display_dataset_info(training_data=None, training_labels=None, test_labels=None):
+    # Explore your dataset 
+    m_train = training_data.shape[0]
+    num_px = training_data.shape[1]
+    m_test = training_data.shape[0]
+
+    print ("Number of training examples: " + str(m_train))
+    print ("Number of testing examples: " + str(m_test))
+    print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+    print ("train_x_orig shape: " + str(training_data.shape))
+    print ("train_y shape: " + str(training_labels.shape))
+    print ("test_x_orig shape: " + str(training_data.shape))
+    print ("test_y shape: " + str(test_labels.shape))
+
+
+def flatten_image_data(original_images=None):
+    # Reshape the training and test examples 
+    flattened_data = original_images.reshape(original_images.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
+
+    # Standardize data to have feature values between 0 and 1.
+    flattened_image_features = flattened_data/255.
+
+    return flattened_image_features
+
+
+def test_external_image(num_pixels, parameters, classifications, model):
+    import imageio.v2 as imageio
+    import PIL
+
+    my_image = "IMG_2296.jpg" # change this to the name of your image file 
+    my_label_y = [1] # the true class of your image (1 -> cat, 0 -> non-cat)
+
+    fname = "images/" + my_image
+    image = np.array(imageio.imread(fname))
+    my_image = np.array(PIL.Image.fromarray(image).resize((num_pixels, num_pixels))).reshape((num_pixels*num_pixels*3,1))
+    my_predicted_image = model.predict(my_image, my_label_y, parameters)
+
+    plt.imshow(image)
+    print ("y = " + str(np.squeeze(my_predicted_image)) + ", your L-layer model predicts a \"" + classifications[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
+    plt.show()
+
+def train(configs=None):
+    
+    # Set default values
+    if configs is None:
+        configs = {}
+        configs['model_class'] = 'n_layer_nn.N_Layer_NN'
+        configs['config'] = {}
+        configs['config']['layers_dims'] = [12288, 20, 7, 5, 1]
+
+    # matplotlib inline
+    plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+    plt.rcParams['image.interpolation'] = 'nearest'
+    plt.rcParams['image.cmap'] = 'gray'
+
+    np.random.seed(55555)
+
+    # Load the data
+    train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+    # show_example_image(index=25, training_data=train_x_orig, training_labels=train_y, classifications=classes)
+    
+    # display_dataset_info(training_data=train_x_orig, training_labels=train_y, test_labels=test_y)
+
+    train_x = flatten_image_data(original_images=train_x_orig)
+    test_x = flatten_image_data(original_images=test_x_orig) 
+    # print ("train_x's shape: " + str(train_x.shape))
+    # print ("test_x's shape: " + str(test_x.shape))
+
+     ### Get model parameters from config file ####
+    if 'n_x' in configs['config']:
+        n_x = configs['config']['n_x']     
+    if 'n_h' in configs['config']:
+        n_h = configs['config']['n_h']
+    if 'n_y' in configs['config']:
+        n_y = configs['config']['n_y']
+    if 'layers_dims' in configs['config']:
+        layers_dims = configs['config']['layers_dims']
+    if 'Two_Layer_NN' in configs['model_class']:
+        layers_dims = (n_x, n_h, n_y)
+
+    # Create a model object based on the config values
+    model = model_factory(configs['model_class'])
+
+    # Train the model
+    parameters = model.model(train_x, train_y, layers_dims, num_iterations = 2500, print_cost = True)
+    # Evaluate the model based on train data and then test data
+    pred_train = model.predict(train_x, train_y, parameters)
+    pred_test = model.predict(test_x, test_y, parameters)
+
+    # Test an image external to train and test data
+    num_px = train_x_orig.shape[1]
+    test_external_image(num_pixels=num_px, parameters=parameters, classifications=classes, model=model)
+
+def model_factory(model_class:str)->object:
+    module_name, class_name = model_class.split('.', -1)
+    module = importlib.import_module(module_name)
+    class_ = getattr(module, class_name)
+    return class_()
+
+def main():
+    # Initialize parser
+    msg = "Executes image classification to identify cat vs non-cat images."
+    parser = argparse.ArgumentParser(description=msg)
+     
+    # Adding optional argument
+    parser.add_argument("-c", "--Config", help = "Configuration file path")
+    
+    # Read arguments from command line
+    args = parser.parse_args()
+    
+    # Parse yaml to obtain config values as dict
+    config_values = None
+    if args.Config:
+        print(f"Configuration file path: {args.Config}")
+        with open(args.Config, 'r') as f:
+            config_values = yaml.load(f, Loader=yaml.FullLoader)
+
+    # Train the model, evaluate, and sample the trained model
+    train(config_values)
+
+if __name__ == '__main__':
+    main()
---
+++ feed_forward/image_classification/n_layer_nn.py
@@ -0,0 +1,133 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import dnn_utils
+
+class N_Layer_NN():
+    def predict(self, X, y, parameters):
+        return dnn_utils.predict(X, y, parameters, dnn_utils.n_model_forward)
+
+    def initialize_parameters_deep(self, layer_dims):
+        """
+        Arguments:
+        layer_dims -- python array (list) containing the dimensions of each layer in our network
+        
+        Returns:
+        parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                        Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+                        bl -- bias vector of shape (layer_dims[l], 1)
+        """
+        
+        np.random.seed(1)
+        parameters = {}
+        L = len(layer_dims)            # number of layers in the network
+
+        for l in range(1, L):
+            parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) / np.sqrt(layer_dims[l-1]) #*0.01
+            parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+            
+            assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
+            assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+            
+        return parameters
+
+    def n_model_backward(self, AL, Y, caches):
+        """
+        Implement the backward propagation for the [LINEAR->RELU] * (L-1) -> LINEAR -> SIGMOID group
+        
+        Arguments:
+        AL -- probability vector, output of the forward propagation (L_model_forward())
+        Y -- true "label" vector (containing 0 if non-cat, 1 if cat)
+        caches -- list of caches containing:
+                    every cache of linear_activation_forward() with "relu" (there are (L-1) or them, indexes from 0 to L-2)
+                    the cache of linear_activation_forward() with "sigmoid" (there is one, index L-1)
+        
+        Returns:
+        grads -- A dictionary with the gradients
+                grads["dA" + str(l)] = ... 
+                grads["dW" + str(l)] = ...
+                grads["db" + str(l)] = ... 
+        """
+        grads = {}
+        L = len(caches) # the number of layers
+        m = AL.shape[1]
+        Y = Y.reshape(AL.shape) # after this line, Y is the same shape as AL
+        
+        # Initializing the backpropagation
+        dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+        
+        # Lth layer (SIGMOID -> LINEAR) gradients. Inputs: "AL, Y, caches". Outputs: "grads["dAL"], grads["dWL"], grads["dbL"]
+        current_cache = caches[L-1]
+        grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = dnn_utils.linear_activation_backward(dAL, current_cache, activation = "sigmoid")
+        
+        for l in reversed(range(L-1)):
+            # lth layer: (RELU -> LINEAR) gradients.
+            current_cache = caches[l]
+            dA_prev_temp, dW_temp, db_temp = dnn_utils.linear_activation_backward(grads["dA" + str(l + 2)], current_cache, activation = "relu")
+            grads["dA" + str(l + 1)] = dA_prev_temp
+            grads["dW" + str(l + 1)] = dW_temp
+            grads["db" + str(l + 1)] = db_temp
+
+        return grads
+
+    def model(self, X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):#lr was 0.009
+        """
+        Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
+        
+        Arguments:
+        X -- data, numpy array of shape (number of examples, num_px * num_px * 3)
+        Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+        layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
+        learning_rate -- learning rate of the gradient descent update rule
+        num_iterations -- number of iterations of the optimization loop
+        print_cost -- if True, it prints the cost every 100 steps
+        
+        Returns:
+        parameters -- parameters learnt by the model. They can then be used to predict.
+        """
+
+        np.random.seed(1)
+        costs = []                         # keep track of cost
+        
+        # Parameters initialization.
+        ### START CODE HERE ###
+        parameters = self.initialize_parameters_deep(layers_dims)
+        ### END CODE HERE ###
+        
+        # Loop (gradient descent)
+        for i in range(0, num_iterations):
+
+            # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+            ### START CODE HERE ### (≈ 1 line of code)
+            AL, caches = dnn_utils.n_model_forward(X, parameters)
+            ### END CODE HERE ###
+            
+            # Compute cost.
+            ### START CODE HERE ### (≈ 1 line of code)
+            cost = dnn_utils.compute_cost(AL, Y)
+            ### END CODE HERE ###
+        
+            # Backward propagation.
+            ### START CODE HERE ### (≈ 1 line of code)
+            grads = self.n_model_backward(AL, Y, caches)
+            ### END CODE HERE ###
+    
+            # Update parameters.
+            ### START CODE HERE ### (≈ 1 line of code)
+            parameters = dnn_utils.update_parameters(parameters, grads, learning_rate)
+            ### END CODE HERE ###
+                    
+            # Print the cost every 100 training example
+            if print_cost and i % 100 == 0:
+                print ("Cost after iteration %i: %f" %(i, cost))
+            if print_cost and i % 100 == 0:
+                costs.append(cost)
+                
+        # plot the cost
+        plt.plot(np.squeeze(costs))
+        plt.ylabel('cost')
+        plt.xlabel('iterations (per tens)')
+        plt.title("Learning rate =" + str(learning_rate))
+        plt.show()
+        
+        return parameters
