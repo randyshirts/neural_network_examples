9b5befbf512139d69cee1c1cc6d40ba7a0c78ef0
randyshirts <randy.shirts@gmail.com>
2022-12-21 21:03:46 -0700

    Added learning rate and num iterations to configs

--- feed_forward/image_classification/image_classification.py
+++ feed_forward/image_classification/image_classification.py
@@ -95,6 +95,9 @@ def train(configs: dict = None) -> None:
         configs["model_class"] = "n_layer_nn.N_Layer_NN"
         configs["config"] = {}
         configs["config"]["layers_dims"] = [12288, 20, 7, 5, 1]
+        configs["config"]["learning_rate"] = 0.0075
+        configs["config"]["num_iterations"] = 3000
+        configs["config"]["print_cost"] = False
 
     # matplotlib inline
     plt.rcParams["figure.figsize"] = (5.0, 4.0)  # set default size of plots
@@ -116,14 +119,21 @@ def train(configs: dict = None) -> None:
     # print ("test_x's shape: " + str(test_x.shape))
 
     # Get model parameters from config file
-    if "n_x" in configs["config"]:
-        n_x = configs["config"]["n_x"]
-    if "n_h" in configs["config"]:
-        n_h = configs["config"]["n_h"]
-    if "n_y" in configs["config"]:
-        n_y = configs["config"]["n_y"]
-    if "layers_dims" in configs["config"]:
-        layers_dims = configs["config"]["layers_dims"]
+    model_config = configs["config"]
+    if "learning_rate" in model_config:
+        lr = model_config["learning_rate"]
+    if "num_iterations" in model_config:
+        num_iterations = model_config["num_iterations"]
+    if "print_cost" in model_config:
+        print_cost = model_config["print_cost"]
+    if "n_x" in model_config:
+        n_x = model_config["n_x"]
+    if "n_h" in model_config:
+        n_h = model_config["n_h"]
+    if "n_y" in model_config:
+        n_y = model_config["n_y"]
+    if "layers_dims" in model_config:
+        layers_dims = model_config["layers_dims"]
     if "Two_Layer_NN" in configs["model_class"]:
         layers_dims = (n_x, n_h, n_y)
 
@@ -132,7 +142,12 @@ def train(configs: dict = None) -> None:
 
     # Train the model
     parameters = model.model(
-        train_x, train_y, layers_dims, num_iterations=2500, print_cost=True
+        train_x,
+        train_y,
+        layers_dims,
+        learning_rate=lr,
+        num_iterations=num_iterations,
+        print_cost=print_cost,
     )
     # Evaluate the model based on train data and then test data
     _ = model.predict(train_x, train_y, parameters)
